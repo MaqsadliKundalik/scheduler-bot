f9e69e75269a8208b76985a04efb4e99773b68e0
Odilbek <=>
2025-12-20 23:16:59 +0500

    c

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from asyncio import create_task, run
 from logging  import basicConfig, INFO
 from aiogram.client.default import DefaultBotProperties
 from middlewares import CheckSUbChannelMiddleware
+from tortoise import Tortoise
 
 basicConfig(level=INFO)
 
@@ -18,7 +19,10 @@ async def main():
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(protect_content=True))
     await init_db()
     create_task(tasks.send_posts_task(bot))
-    await dp.start_polling(bot)
-
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await Tortoise.close_connections()
+        
 if __name__ == "__main__":
     run(main())
